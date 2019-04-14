e0a8f62403393e497b1305f8016bbc9183a0c6a7
Fabio Hiroki <fabiothiroki@gmail.com>
2019-04-14 10:47:23 -0300

    Remove comments

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -36,9 +36,6 @@ public class Main {
   @Value("${spring.datasource.url}")
   private String dbUrl;
 
-//  @Autowired
-//  private DataSource dataSource;
-
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
   }
@@ -50,24 +47,6 @@ public class Main {
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
-
-//    try (Connection connection = dataSource.getConnection()) {
-//      Statement stmt = connection.createStatement();
-//      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-//      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-//      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-//
-//      ArrayList<String> output = new ArrayList<String>();
-//      while (rs.next()) {
-//        output.add("Read from DB: " + rs.getTimestamp("tick"));
-//      }
-//
-//      model.put("records", output);
-//      return "db";
-//    } catch (Exception e) {
-//      model.put("message", e.getMessage());
-//      return "error";
-//    }
     ObjectMapper mapper = new ObjectMapper();
     TypeReference<List<Object>> typeReference = new TypeReference<List<Object>>(){};
     InputStream inputStream = TypeReference.class.getResourceAsStream("/pokedex.json");
@@ -80,16 +59,4 @@ public class Main {
       return "error";
     }
   }
-
-//  @Bean
-//  public DataSource dataSource() throws SQLException {
-//    if (dbUrl == null || dbUrl.isEmpty()) {
-//      return new HikariDataSource();
-//    } else {
-//      HikariConfig config = new HikariConfig();
-//      config.setJdbcUrl(dbUrl);
-//      return new HikariDataSource(config);
-//    }
-//  }
-
 }
